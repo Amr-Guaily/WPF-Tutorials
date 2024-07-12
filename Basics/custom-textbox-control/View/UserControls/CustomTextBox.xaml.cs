d8464ae5b8301f71e0364a587c4d4d7ab1fccc83
Amr-Guaily <amrguaily@gmail.com>
2024-07-12 21:26:29 +0300

    custom control

---
+++ Basics/custom-textbox-control/View/UserControls/CustomTextBox.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace custom_textbox_control.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for CustomTextBox.xaml
+    /// </summary>
+    public partial class CustomTextBox : UserControl
+    {
+        public CustomTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private string placeholder;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set
+            {
+                placeholder = value;
+                // don not do this, WHY!!
+                // This should be done with something called "onPropertyChanged"
+                tbPlaceholder.Text = placeholder;
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            textInput.Clear();
+            textInput.Focus();
+        }
+
+        private void textInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(textInput.Text)) 
+            {
+                tbPlaceholder.Visibility = Visibility.Visible;
+            } else
+            {
+                tbPlaceholder.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
